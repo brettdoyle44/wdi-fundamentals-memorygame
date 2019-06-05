c19c40e0bc526e0e2cd16ac9d77cd8b6c8432df5
Brett Doyle <brettdoyle44@gmail.com>
2019-06-05 12:17:53 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,27 @@
-var cardOne = "queen";
+var cards = ["queen","queen","king","king"];
 
-var cardTwo = "queen";
+var cardsInPlay = [];
 
-var cardThree = "king";
+var cardOne = cards[0];
 
-var cardFour = "king";
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
+console.log("User flipped" + " " + cardsInPlay);
 
-console.log("User flipped " + cardThree);
+var cardTwo = cards [1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay [1]){
+	alert("You found a match!")
+}
+
+	else if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry try again");
+
+	}
+}
+
